7ec1a76be11a53d9b741d26473476cfb1897b863
harmlessprince <realolamilekan@gmail.com>
2020-05-24 12:42:27 +0100

    User and tour Controller created

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "success",
+    message: "Route not yet defined",
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: "success",
+    message: "Route not yet defined",
+  });
+};
+
+exports.updateUser = (res, req) => {
+  res.status(500).json({
+    status: "success",
+    message: "Route not yet defined",
+  });
+};
+
+exports.deleteUser = (res, req) => {
+  res.status(500).json({
+    status: "success",
+    message: "Route not yet defined",
+  });
+};
+
+exports.showUser = (res, req) => {
+  res.status(500).json({
+    status: "success",
+    message: "Route not yet defined",
+  });
+};
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,44 +1,17 @@
-const express = require("express")
+const express = require("express");
 
 const router = express.Router();
 
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "success",
-    message: "Route not yet defined",
-  });
-};
+const userController = require("./../controllers/userController");
 
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: "success",
-    message: "Route not yet defined",
-  });
-};
-
-const updateUser = (res, req) => {
-  res.status(500).json({
-    status: "success",
-    message: "Route not yet defined",
-  });
-};
-
-const deleteUser = (res, req) => {
-  res.status(500).json({
-    status: "success",
-    message: "Route not yet defined",
-  });
-};
-
-const showUser = (res, req) => {
-  res.status(500).json({
-    status: "success",
-    message: "Route not yet defined",
-  });
-};
-
-router.route("/").get(getAllUsers).post(createUser);
-router.route("/:id").patch(updateUser).delete(deleteUser).get(showUser);
-
+router
+  .route("/")
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+router
+  .route("/:id")
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser)
+  .get(userController.showUser);
 
 module.exports = router;
